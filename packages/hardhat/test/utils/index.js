ec676ff38747cca4408063b8bc3837358be7210e
dan13ram <dan13ram@gmail.com>
2022-03-25 08:40:46 +0530

    added tests for erc165

--- packages/hardhat/test/utils/index.js
+++ packages/hardhat/test/utils/index.js
@@ -1,10 +1,12 @@
 const snapshot = require("./snapshot.js");
 const eip712 = require("./eip712.js");
+const erc165 = require("./erc165.js");
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 module.exports = {
   sleep,
-  eip712,
+  ...eip712,
+  ...erc165,
   ...snapshot,
 };
