5b30de1e7fcdcd1347a9ea3285875fa57b70a1c6
dan13ram <dan13ram@gmail.com>
2022-03-25 06:53:20 +0530

    added tests for addCron

--- packages/hardhat/test/test.js
+++ packages/hardhat/test/test.js
@@ -10,6 +10,7 @@ const { constants } = ethers;
 describe("PoignART", function () {
   let poignart;
   let signers;
+  let DEFAULT_ADMIN_ROLE;
   let MINTER_ROLE;
   let CRON_JOB;
   let snapshot;
@@ -24,9 +25,13 @@ describe("PoignART", function () {
     const PoignART = await ethers.getContractFactory("PoignART");
     poignart = await PoignART.deploy();
 
+    DEFAULT_ADMIN_ROLE = await poignart.DEFAULT_ADMIN_ROLE();
     MINTER_ROLE = await poignart.MINTER_ROLE();
     CRON_JOB = await poignart.CRON_JOB();
 
+    expect(
+      await poignart.hasRole(DEFAULT_ADMIN_ROLE, signers[0].address)
+    ).to.equal(true);
     expect(await poignart.hasRole(MINTER_ROLE, signers[0].address)).to.equal(
       true
     );
@@ -63,16 +68,38 @@ describe("PoignART", function () {
 
     it("Should verify artist in merkle tree", async function () {
       const addresses = signers.slice(0, 3).map((s) => s.address);
-      snapshot = getSnapshot(addresses);
       const proof = snapshot.getMerkleProof(addresses[0]);
       expect(await poignart._verifyArtist(addresses[0], proof)).to.equal(true);
     });
 
     it("Should not verify artist not in merkle tree", async function () {
       const addresses = signers.slice(0, 4).map((s) => s.address);
-      snapshot = getSnapshot(addresses);
+      const snapshot = getSnapshot(addresses);
       const proof = snapshot.getMerkleProof(addresses[3]);
       expect(await poignart._verifyArtist(addresses[3], proof)).to.equal(false);
+    });
+
+    it("Should not add new user to CRON_JOB role if not admin", async function () {
+      const receipt = poignart.connect(signers[1]).addCron(signers[1].address);
+      await expect(receipt).to.revertedWith(
+        `AccessControl: account ${signers[1].address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`
+      );
+    });
+
+    it("Should add new user to CRON_JOB role & update root", async function () {
+      expect(await poignart.hasRole(CRON_JOB, signers[1].address)).to.equal(
+        false
+      );
+      await poignart.addCron(signers[1].address);
+      expect(await poignart.hasRole(CRON_JOB, signers[1].address)).to.equal(
+        true
+      );
+
+      const addresses = signers.slice(0, 5).map((s) => s.address);
+      snapshot = getSnapshot(addresses);
+      const root = snapshot.getMerkleRoot();
+      await poignart.connect(signers[1]).cronJobRoot(root);
+      expect(await poignart._merkleRoot()).to.equal(root);
     });
   });
 });
